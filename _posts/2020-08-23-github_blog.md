018f0b3629a36458082ba127e10709768f977d84
manju-minji <0923alswl@gmail.com>
2020-08-23 15:40:03 +0900

    기본 설정 &테스트 글 작성

---
+++ _posts/2020-08-23-github_blog.md
@@ -0,0 +1,23 @@
+---
+title: "Github 블로그 만들기"
+comments: true
+categories:
+  - github blog
+tags:
+  - github
+  - jekyll
+show_date: true
+last_modified_at: 2020-08-23
+---
+
+### 발생 가능한 오류
+
+* incompatible character encodings: ASCII-8BIT and UTF-8
+
+post의 이름 형식은 **년도-월-일-제목.형식**이여야 한다.
+
+이때, 제목이 영어가 아니라 한국어로 작성 되어 있으면
+
+---<cite>incompatible character encodings: ASCII-8BIT and UTF-8</cite>
+
+이라는 에러가 나니까 주의하도록 하자.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Github 블로그 만들기",
+        "excerpt":"발생 가능한 오류      incompatible character encodings: ASCII-8BIT and UTF-8   post의 이름 형식은 년도-월-일-제목.형식이여야 한다.   이때, 제목이 영어가 아니라 한국어로 작성 되어 있으면   —incompatible character encodings: ASCII-8BIT and UTF-8   이라는 에러가 나니까 주의하도록 하자.  ","categories": ["github blog"],
+        "tags": ["github","jekyll"],
+        "url": "http://localhost:4000/github%20blog/github_blog/",
+        "teaser": null
+      },{
+        "title": "머신러닝이 뭔지 알고 싶은 사람들에게 머신러닝 야학을!",
+        "excerpt":"Info Button Text 4차 산업 혁명의 시대를 맞이하여 머신러닝이라는 단어는 누구나 한번쯤은 들어봤을 법한 단어로 자리매김했다. 하지만 ‘머신러닝이 뭐야?’라는 질문에 대답을 할 수 있는 사람은 과연 몇이나 될까…? 이 질문에 답을 할 수 있고, 더 나아가 전공자 수준까지의 지식을 무료로 얻을 수 있는 컨텐츠가 나왔고 그것이 바로 생활 코딩에서 운영하는...","categories": ["Machine Learning"],
+        "tags": ["Machine Learning","생활 코딩"],
+        "url": "http://localhost:4000/machine%20learning/machine_learing_yahak/",
+        "teaser": null
+      }]
